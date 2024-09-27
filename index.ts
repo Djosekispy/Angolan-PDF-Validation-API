e163f2e1878386bf8acbccb08c3e58afdbfe5f81
Djosekispy <globof129@gmail.com>
2024-09-27 14:54:05 +0100

    Arquivo de configuração para documentação com swegger

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { corsMiddleware } from './src/middleware/cors';
 import userRoutes from './src/routes';
 import { SERVER_PORT } from './src/config/variables.env';
 import errorHandler from './src/middleware/errorHandler';
+import { setupSwagger } from './src/config/swaggerConfig';
 
 const app = express();
 const port = SERVER_PORT;
@@ -11,6 +12,8 @@ app.use(corsMiddleware);
 app.use(express.json());
 app.use(errorHandler);
 app.use(userRoutes);
+
+setupSwagger(app);
 
 app.listen(port,async ()=>{
  console.log(`Servidor rodando  na porta: ${port}`);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,153 @@
-import express from 'express'
-import { validatorController } from './bootstrap';
+import express from 'express';
 import { saveFilePathMiddleware, uploadFile } from '../middleware/upload';
 import { ReadingFile } from '../middleware/ValidateProfTranser';
 import { ReadingAndExtractDataFromImage } from '../middleware/BiProcessFile';
+import { validatorController } from './bootstrap';
 
 const userRoutes = express.Router();
 
+/**
+ * @swagger
+ * /profbank/save:
+ *   post:
+ *     summary: Valida um comprovante de transferência bancária
+ *     description: Faz upload de um comprovante de transferência bancária e valida os dados.
+ *     tags:
+ *       - Transferência Bancária
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               file:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       200:
+ *         description: Comprovante validado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Arquivo validado com sucesso"
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: string
+ *                         example: "64220965-9eb6-4b3e-96b7-7b59becee897"
+ *                       datetime:
+ *                         type: string
+ *                         example: "xxxx-xx-xx xx:xx:xx"
+ *                       destination:
+ *                         type: string
+ *                         example: "xxxxxxxxxx"
+ *                       iban:
+ *                         type: string
+ *                         example: "AO06.xxxx.xxxx.xxxx.xxxx.xxxx.x"
+ *                       type:
+ *                         type: string
+ *                         example: "Transferência Bancária"
+ *                       transactionCode:
+ *                         type: number
+ *                         example: xxxxxxxx
+ *                       amount:
+ *                         type: number
+ *                         example: xxxxxxxx
+ *       400:
+ *         description: Erro interno do servidor
+ */
 
-userRoutes.post('/profbank/save', uploadFile,saveFilePathMiddleware,ReadingFile,validatorController.validateBankReceipt);
-userRoutes.post('/bidoc/save', uploadFile,saveFilePathMiddleware,ReadingAndExtractDataFromImage,validatorController.validateBiDoc);
+userRoutes.post('/profbank/save', uploadFile, saveFilePathMiddleware, ReadingFile, validatorController.validateBankReceipt);
+
+/**
+ * @swagger
+ * /bidoc/save:
+ *   post:
+ *     summary: Valida um documento de BI
+ *     description: Faz upload de um arquivo de BI e extrai os dados utilizando OCR.
+ *     tags:
+ *       - BI
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               file:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       200:
+ *         description: Documento BI validado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Imagem processada e BI salvo com sucesso"
+ *                 gptResponse:
+ *                   type: object
+ *                   properties:
+ *                     country:
+ *                       type: string
+ *                       example: "REPÚBLICA DE ANGOLA"
+ *                     identificationCard:
+ *                       type: object
+ *                       properties:
+ *                         fullName:
+ *                           type: string
+ *                           example: "xxxxxxxxxx"
+ *                         fatherName:
+ *                           type: string
+ *                           example: "xxxxxxxxxx"
+ *                         motherName:
+ *                           type: string
+ *                           example: "xxxxxxxxxx"
+ *                         cardNumber:
+ *                           type: string
+ *                           example: "xxxxxxxxxx"
+ *                         residence:
+ *                           type: string
+ *                           example: "CASA S/Nº xxxxxxxx"
+ *                         birthPlace:
+ *                           type: string
+ *                           example: "xxxxxx"
+ *                         province:
+ *                           type: string
+ *                           example: "xxxxxx"
+ *                         birthDate:
+ *                           type: string
+ *                           example: "xx/xx/xxxx"
+ *                         sex:
+ *                           type: string
+ *                           example: "xxxxxxxx"
+ *                         height:
+ *                           type: string
+ *                           example: "175"
+ *                         maritalStatus:
+ *                           type: string
+ *                           example: "xxxxxxxxx"
+ *                         issueDate:
+ *                           type: string
+ *                           example: "xx/xx/xxxx"
+ *                         expiryDate:
+ *                           type: string
+ *                           example: "xx/xx/xxxx"
+ *       400:
+ *         description: Erro interno do servidor
+ */
+
+userRoutes.post('/bidoc/save', uploadFile, saveFilePathMiddleware, ReadingAndExtractDataFromImage, validatorController.validateBiDoc);
 
 export default userRoutes;
